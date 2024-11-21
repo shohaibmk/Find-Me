df430c0fd5c95b1f2b767ffb9791a098c65209e4
Shohaib Mallick <shohaibm99@gmail.com>
2024-11-21 17:05:15 -0500

    GET

--- Part 2/server.py
+++ Part 2/server.py
@@ -1,33 +1,105 @@
 from flask import Flask, request, jsonify
 from threading import Lock
-import time
+from datetime import datetime
 import logging
+
+#in memory Database
+database = dict()
+product_number = 1  #product counter
+
+#method to write into the in memory database
+def databaseWrite(name,description,dimensions,color,price,currency,image):
+    global product_number
+    # print("Writing in DB")
+    insert_data = {
+        "name":name,
+        "description": description,
+        "dimensions": dimensions,
+        "color": color,
+        "price": price,
+        "currency": currency,
+        "images":image
+    }
+    print("Product Details:\n",insert_data)
+    key = "product" + str(product_number)
+    # print("after")
+    database[key] = insert_data
+    product_number+=1
+    return key
+
+def databaseRead(pid):
+    # record = database[pid]
+    try:
+        return database[pid]
+    except KeyError:
+        print("key not found")
+        return None
+
+def get_and_format_product(pid):
+    record =  databaseRead(pid)
+    if not record:
+        print("record not found")
+        return None
+    else:
+        print("record found")
+        return "Found"
+
+
 
 app = Flask(__name__)
 
-# Configure logging
-# logging.basicConfig(level=logging.DEBUG)  # Set log level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
-# logger = logging.getLogger(__name__)
-
-
+#method: POST
+#route: '/products'
 @app.route('/products', methods=['POST'])
-def add_product():                                          #Adding new product
-    if request.headers.get('Content-Type') == 'application/json':              #posting JSON Object
-        app.logger.info('JSON incoming')
-        data = request.json
-    if request.headers.get('Content-Type') == 'application/text': 
-        app.logger.info('Text incoming')
-        data = request.get_data()
-    # app.logger.info('A value for debugging'+str(data))
-    # logger.info('hola',data)
-    return jsonify({"hola":"world","data":str(data)}), 201
+def add_product():  # Adding new product
+    print("\nWrite initiated |",datetime.now())
+    try:
+        data = request.form
+        list_of_files = []
 
+        #check for complete form data
+        if not data or len(data) !=6:
+            return jsonify({"Error":"Incomplete request","Message":"Make sure to include name,description,dimensions,color,price,currency"}),400
+
+        #check for complete image files
+        if request.files:        
+            for file in request.files:
+                list_of_files.append(request.files[file])
+
+            # print("\n\nlist of images:",list_of_files)
+        elif not request.files:
+            return jsonify({"Error":"Incomplete request","Message":"Missing product images"}),400
+                
+        try:
+            pid = databaseWrite(data['Name'],data['Description'],data['Dimensions'],data['Color'],data['Currency'],data['Price'],list_of_files)
+        except:
+            print("Write Failed |",datetime.now())
+            return jsonify({"Error":"Write Unsuccessful","Message":"Error creating new product DB"}),400
+        print("Write Successful |",datetime.now())
+        return jsonify({"Message":"Write Successful" ,"Data": str(data),"Images":str(list_of_files),"ProductID":pid}), 201
+    except:
+        print("Write Failed |",datetime.now())
+        return jsonify({"Error":"Write Unsuccessful","Message":"Error creating new product"})
     
-@app.route('/products',methods = ['GET'])
+
+
+
+#method: POST
+#route: '/products'
+#query parameters: pid
+@app.route('/products', methods=['GET'])
 def get_product():
-    return "This product",201
-    
+    pid = request.args.get('pid')
+    if pid:
+        pid = request.args.get('pid')
+        print(request.args.get('pid'))
+        product = get_and_format_product(pid)
+        return jsonify({"pid":pid}), 201
+    else:    
+        
+        return jsonify({"Message":"Unknown Request"}), 400
+
 if __name__ == '__main__':
-    app.run(debug=True,port = 5000)
-    # print("\n\n\n\n\n\n\n\n\n")
-    # app.logger.debug("\n\n\n\n\n\n\n\n\n")
+    app.run(debug=True, port=5001)
+    print("\n\n\n\n\n\n")
+
